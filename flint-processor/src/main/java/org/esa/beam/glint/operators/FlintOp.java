88230c486bbcbf91f5265c74c6ed4c34da73f986
marpet <marco.peters@brockmann-consult.de>
2011-05-03 16:03:01 +0200

    - updated to latest version of dependencies - removed calls to deprecated methods

--- flint-processor/src/main/java/org/esa/beam/glint/operators/FlintOp.java
+++ flint-processor/src/main/java/org/esa/beam/glint/operators/FlintOp.java
@@ -27,14 +27,14 @@ import java.util.logging.Logger;
  * @version $Revision: 5451 $ $Date: 2009-06-05 18:36:49 +0200 (Fr, 05 Jun 2009) $
  */
 @OperatorMetadata(alias = "glint.Flint",
-        version = "1.0-SNAPSHOT",
-        authors = "Olaf Danne",
-        copyright = "(c) 2009 by Brockmann Consult",
-        description = "Flint Processor.")
+                  version = "1.0-SNAPSHOT",
+                  authors = "Olaf Danne",
+                  copyright = "(c) 2009 by Brockmann Consult",
+                  description = "Flint Processor.")
 public class FlintOp extends Operator {
 
     @SourceProduct(alias = "l1bCollocate",
-            description = "MERIS/AATSR collocation product.")
+                   description = "MERIS/AATSR collocation product.")
     private Product collocateProduct;
 
     @TargetProduct(description = "The target product.")
@@ -76,7 +76,7 @@ public class FlintOp extends Operator {
                label = "Effective Windspeed 2")
     boolean writeEffectiveWindspeed2;
 
-     @Parameter(defaultValue = "false",
+    @Parameter(defaultValue = "false",
                label = "Radiance 1")
     boolean writeRadiance1;
 
@@ -96,9 +96,9 @@ public class FlintOp extends Operator {
     private Band invalidBand;
 
     /* AATSR L1 Cloud Flags (just the ones needed) */
-    final int AATSR_L1_CF_LAND      = 0;
-    final int AATSR_L1_CF_CLOUDY    = 1;
-    final int AATSR_L1_CF_SUNGLINT  = 2;
+    final int AATSR_L1_CF_LAND = 0;
+    final int AATSR_L1_CF_CLOUDY = 1;
+    final int AATSR_L1_CF_SUNGLINT = 2;
 
     public static final String CONFID_NADIR_FLAGS = "confid_flags_nadir_S";
     public static final String CONFID_FWARD_FLAGS = "confid_flags_fward_S";
@@ -130,7 +130,7 @@ public class FlintOp extends Operator {
     private FlintSolarPart37 solarPart37;
     private FlintSolarPart37WaterVapour solarPart37WaterVapour;
     private FlintGeometricalConversion geometricalConversion;
-    
+
     private float solarIrradiance37;
 
     private Logger logger;
@@ -160,8 +160,8 @@ public class FlintOp extends Operator {
 
         // get solar irradiance for day of year
         String startTime = collocateProduct.getMetadataRoot().getElement(
-                        "MPH").getAttribute("PRODUCT")
-                        .getData().getElemString().substring(14);     // e.g., 20030614
+                "MPH").getAttribute("PRODUCT")
+                .getData().getElemString().substring(14);     // e.g., 20030614
 
         final int dayOfYear = preparation.getDayOfYear(startTime);
 
@@ -171,9 +171,9 @@ public class FlintOp extends Operator {
         // set up tiles for MERIS and AATSR which cover the whole scene...
         int sceneWidth = collocateProduct.getSceneRasterWidth();
         int sceneHeight = collocateProduct.getSceneRasterHeight();
-        Rectangle rect = new Rectangle(0,0, sceneWidth, sceneHeight);
-        vaMerisTileComplete = getSourceTile(collocateProduct.getTiePointGrid("view_azimuth"), rect, null);
-        vaAatsrNadirTileComplete = getSourceTile(collocateProduct.getBand("view_azimuth_nadir_S"), rect, null);
+        Rectangle rect = new Rectangle(0, 0, sceneWidth, sceneHeight);
+        vaMerisTileComplete = getSourceTile(collocateProduct.getTiePointGrid("view_azimuth"), rect);
+        vaAatsrNadirTileComplete = getSourceTile(collocateProduct.getBand("view_azimuth_nadir_S"), rect);
 
         // correct azimuths in these tiles for later usage...
         preparation.correctViewAzimuthLinear(vaMerisTileComplete, rect);
@@ -184,7 +184,7 @@ public class FlintOp extends Operator {
     // This method creates the target product
     //
     private void createTargetProduct() {
-         String productType = collocateProduct.getProductType();
+        String productType = collocateProduct.getProductType();
         String productName = collocateProduct.getName();
         int sceneWidth = collocateProduct.getSceneRasterWidth();
         int sceneHeight = collocateProduct.getSceneRasterHeight();
@@ -300,25 +300,25 @@ public class FlintOp extends Operator {
         pm.beginTask("Processing frame...", rectangle.height);
 
         try {
-            Tile szMerisTile = getSourceTile(collocateProduct.getTiePointGrid("sun_zenith"), rectangle, pm);
-            Tile vzMerisTile = getSourceTile(collocateProduct.getTiePointGrid("view_zenith"), rectangle, pm);
-            Tile saMerisTile = getSourceTile(collocateProduct.getTiePointGrid("sun_azimuth"), rectangle, pm);
-            Tile zonalWindTile = getSourceTile(collocateProduct.getTiePointGrid("zonal_wind"), rectangle, pm);
-            Tile meridWindTile = getSourceTile(collocateProduct.getTiePointGrid("merid_wind"), rectangle, pm);
+            Tile szMerisTile = getSourceTile(collocateProduct.getTiePointGrid("sun_zenith"), rectangle);
+            Tile vzMerisTile = getSourceTile(collocateProduct.getTiePointGrid("view_zenith"), rectangle);
+            Tile saMerisTile = getSourceTile(collocateProduct.getTiePointGrid("sun_azimuth"), rectangle);
+            Tile zonalWindTile = getSourceTile(collocateProduct.getTiePointGrid("zonal_wind"), rectangle);
+            Tile meridWindTile = getSourceTile(collocateProduct.getTiePointGrid("merid_wind"), rectangle);
 
-            Tile seAatsrNadirTile = getSourceTile(collocateProduct.getBand("sun_elev_nadir_S"), rectangle, pm);
-            Tile veAatsrNadirTile = getSourceTile(collocateProduct.getBand("view_elev_nadir_S"), rectangle, pm);
-            Tile saAatsrNadirTile = getSourceTile(collocateProduct.getBand("sun_azimuth_nadir_S"), rectangle, pm);
-            Tile cfAatsrNadirTile = getSourceTile(collocateProduct.getBand("cloud_flags_nadir_S"), rectangle, pm);
+            Tile seAatsrNadirTile = getSourceTile(collocateProduct.getBand("sun_elev_nadir_S"), rectangle);
+            Tile veAatsrNadirTile = getSourceTile(collocateProduct.getBand("view_elev_nadir_S"), rectangle);
+            Tile saAatsrNadirTile = getSourceTile(collocateProduct.getBand("sun_azimuth_nadir_S"), rectangle);
+            Tile cfAatsrNadirTile = getSourceTile(collocateProduct.getBand("cloud_flags_nadir_S"), rectangle);
 
-            Tile merisRad14Tile = getSourceTile(collocateProduct.getBand("radiance_14_M"), rectangle, pm);
-            Tile merisRad15Tile = getSourceTile(collocateProduct.getBand("radiance_15_M"), rectangle, pm);
-            Tile aatsrReflNadir1600Tile = getSourceTile(collocateProduct.getBand("reflec_nadir_1600_S"), rectangle, pm);
-            Tile aatsrBTNadir0370Tile = getSourceTile(collocateProduct.getBand("btemp_nadir_0370_S"), rectangle, pm);
-            Tile aatsrBTNadir1100Tile = getSourceTile(collocateProduct.getBand("btemp_nadir_1100_S"), rectangle, pm);
-            Tile aatsrBTNadir1200Tile = getSourceTile(collocateProduct.getBand("btemp_nadir_1200_S"), rectangle, pm);
+            Tile merisRad14Tile = getSourceTile(collocateProduct.getBand("radiance_14_M"), rectangle);
+            Tile merisRad15Tile = getSourceTile(collocateProduct.getBand("radiance_15_M"), rectangle);
+            Tile aatsrReflNadir1600Tile = getSourceTile(collocateProduct.getBand("reflec_nadir_1600_S"), rectangle);
+            Tile aatsrBTNadir0370Tile = getSourceTile(collocateProduct.getBand("btemp_nadir_0370_S"), rectangle);
+            Tile aatsrBTNadir1100Tile = getSourceTile(collocateProduct.getBand("btemp_nadir_1100_S"), rectangle);
+            Tile aatsrBTNadir1200Tile = getSourceTile(collocateProduct.getBand("btemp_nadir_1200_S"), rectangle);
 
-            Tile isInvalid = getSourceTile(invalidBand, rectangle, pm);
+            Tile isInvalid = getSourceTile(invalidBand, rectangle);
             FlintGeometricalConversion conversion = geometricalConversion.clone();
             FlintSolarPart37WaterVapour waterVapour = solarPart37WaterVapour.clone();
 
@@ -335,15 +335,16 @@ public class FlintOp extends Operator {
                     final float aatsrSunElevation = seAatsrNadirTile.getSampleFloat(x, y);
                     final float aatsrBt37 = aatsrBTNadir0370Tile.getSampleFloat(x, y);
                     if (isInvalid.getSampleBoolean(x, y)
-                     ||   !preparation.isUsefulPixel(cloudFlagNadirLand, cloudFlagNadirCloudy, cloudFlagNadirSunglint, aatsrViewElevation, aatsrBt37))
-                    {
+                        || !preparation.isUsefulPixel(cloudFlagNadirLand, cloudFlagNadirCloudy, cloudFlagNadirSunglint,
+                                                      aatsrViewElevation, aatsrBt37)) {
                         targetTile.setSample(x, y, 0);
                     } else {
 
                         // 1. The solar part of 3.7
                         // 1.a. Thermal extrapolation of 11/12 to 3.7
-                        final float aatsrBTThermalPart37 = 
-                                solarPart37.extrapolateTo37(aatsrBTNadir1100Tile.getSampleFloat(x, y), aatsrBTNadir1200Tile.getSampleFloat(x, y));
+                        final float aatsrBTThermalPart37 =
+                                solarPart37.extrapolateTo37(aatsrBTNadir1100Tile.getSampleFloat(x, y),
+                                                            aatsrBTNadir1200Tile.getSampleFloat(x, y));
 
                         // 1.b.1 Calculation of water vapour
                         final float zonalWind = zonalWindTile.getSampleFloat(x, y);
@@ -351,23 +352,27 @@ public class FlintOp extends Operator {
                         float merisViewAzimuth = vaMerisTileComplete.getSampleFloat(x, y);
                         float merisSunAzimuth = saMerisTile.getSampleFloat(x, y);
                         float merisAzimuthDifference = preparation.removeAzimuthDifferenceAmbiguity(merisViewAzimuth,
-                                    merisSunAzimuth);
+                                                                                                    merisSunAzimuth);
                         final float merisViewZenith = vzMerisTile.getSampleFloat(x, y);
                         final float merisSunZenith = szMerisTile.getSampleFloat(x, y);
                         final float merisRad14 = merisRad14Tile.getSampleFloat(x, y);
                         final float merisRad15 = merisRad15Tile.getSampleFloat(x, y);
 
-                        float waterVapourColumn = waterVapour.computeWaterVapour(zonalWind, meridWind, merisAzimuthDifference,
-                                merisViewZenith, merisSunZenith, merisRad14, merisRad15);
+                        float waterVapourColumn = waterVapour.computeWaterVapour(zonalWind, meridWind,
+                                                                                 merisAzimuthDifference,
+                                                                                 merisViewZenith, merisSunZenith,
+                                                                                 merisRad14, merisRad15);
                         if (targetBand.getName().equals(STEP_1b1_RESULT_NAME)) {
                             targetTile.setSample(x, y, waterVapourColumn);
                         }
 
                         // 1.b.2 Calculation of transmission
                         final float aatsrTrans37 = solarPart37.computeTransmission(37, waterVapourColumn,
-                               90.0f - aatsrSunElevation, 90.0f - aatsrViewElevation);
+                                                                                   90.0f - aatsrSunElevation,
+                                                                                   90.0f - aatsrViewElevation);
                         final float aatsrTrans16 = solarPart37.computeTransmission(16, waterVapourColumn,
-                               90.0f - aatsrSunElevation, 90.0f - aatsrViewElevation);
+                                                                                   90.0f - aatsrSunElevation,
+                                                                                   90.0f - aatsrViewElevation);
                         if (targetBand.getName().equals(STEP_1b2_RESULT1_NAME)) {
                             targetTile.setSample(x, y, aatsrTrans37);
                         }
@@ -376,8 +381,9 @@ public class FlintOp extends Operator {
                         }
 
                         // 1.c Conversion of BT to normalized radiance
-                        final float aatsrRad37 = solarPart37.convertBT2Radiance(aatsrBt37)/solarIrradiance37;
-                        final float aatsrRadianceThermalPart37 = solarPart37.convertBT2Radiance(aatsrBTThermalPart37)/solarIrradiance37;
+                        final float aatsrRad37 = solarPart37.convertBT2Radiance(aatsrBt37) / solarIrradiance37;
+                        final float aatsrRadianceThermalPart37 = solarPart37.convertBT2Radiance(
+                                aatsrBTThermalPart37) / solarIrradiance37;
                         if (targetBand.getName().equals(STEP_1c_RESULT1_NAME)) {
                             targetTile.setSample(x, y, aatsrRad37);
                         }
@@ -394,12 +400,12 @@ public class FlintOp extends Operator {
                         // 1.e Simple additional cloud mask
                         final float aatsrRefl16 = aatsrReflNadir1600Tile.getSampleFloat(x, y);
                         final float aatsrRefl16T = aatsrRefl16 / aatsrTrans16;
-                        
+
 //                        boolean cloud = solarPart37.computeAdditionalCloudMask(aatsrSolarPart37a, aatsrRefl16T);
                         boolean cloud = false; // perhaps activate if needed
 
                         if (targetBand.getName().equals(STEP_1b2_RESULT2_NAME)) {
-                            targetTile.setSample(x, y, aatsrRefl16T/0.79);
+                            targetTile.setSample(x, y, aatsrRefl16T / 0.79);
                         }
                         if (targetBand.getName().equals(STEP_1e_RESULT_NAME)) {
                             if (cloud) {
@@ -438,23 +444,27 @@ public class FlintOp extends Operator {
                             float aatsrViewAzimuth = vaAatsrNadirTileComplete.getSampleFloat(x, y);
                             float aatsrSunAzimuth = saAatsrNadirTile.getSampleFloat(x, y);
 
-                            float aatsrAzimuthDifference = preparation.removeAzimuthDifferenceAmbiguity(aatsrViewAzimuth,
+                            float aatsrAzimuthDifference = preparation.removeAzimuthDifferenceAmbiguity(
+                                    aatsrViewAzimuth,
                                     aatsrSunAzimuth);
 
 
                             final float[][] merisNormalizedRadianceResultMatrix =
                                     conversion.convertAatsrRad37ToMerisRad(aatsrSolarPart37, merisSunZenith,
-                                            merisViewZenith, 180.0f - aatsrAzimuthDifference, 180.0f - merisAzimuthDifference);
+                                                                           merisViewZenith,
+                                                                           180.0f - aatsrAzimuthDifference,
+                                                                           180.0f - merisAzimuthDifference);
 
                             if (targetBand.getName().equals(RESULT_NUMBERWINDSPEEDS_NAME)) {
-                                    int numberWindspeeds = conversion.windspeedFound(merisNormalizedRadianceResultMatrix);
-                                    targetTile.setSample(x, y, numberWindspeeds);
+                                int numberWindspeeds = FlintGeometricalConversion.windspeedFound(
+                                        merisNormalizedRadianceResultMatrix);
+                                targetTile.setSample(x, y, numberWindspeeds);
                             }
 
                             // 2.b Ambiuguity reduction and final output
-                            if (conversion.windspeedFound(merisNormalizedRadianceResultMatrix) > 0) {
-                                final float[] finalResultWindspeedRadiance = conversion.getAmbiguityReducedRadiance
-                                    (merisNormalizedRadianceResultMatrix, zonalWind, meridWind);
+                            if (FlintGeometricalConversion.windspeedFound(merisNormalizedRadianceResultMatrix) > 0) {
+                                final float[] finalResultWindspeedRadiance = FlintGeometricalConversion.getAmbiguityReducedRadiance
+                                        (merisNormalizedRadianceResultMatrix, zonalWind, meridWind);
                                 if (targetBand.getName().equals(RESULT_RADIANCE1_NAME)) {
                                     targetTile.setSample(x, y, merisNormalizedRadianceResultMatrix[0][1]);
                                 }
@@ -493,6 +503,7 @@ public class FlintOp extends Operator {
      * It provides operator meta-data and is a factory for new operator instances.
      */
     public static class Spi extends OperatorSpi {
+
         public Spi() {
             super(FlintOp.class);
         }
